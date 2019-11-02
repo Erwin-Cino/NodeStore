75b6016711ea238a9af1d9500f61251c3e0abc93
Erwin-Cino <ErwinCino0812@gmail.com>
2019-11-03 02:56:36 +0800

    Added get post and patch http methods on product api

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,41 +1,90 @@
 const express = require("express");
 const router = express.Router();
+const Product = require("../models/product");
+const mongoose = require("mongoose");
 
-router.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "Handling GET requests to /products"
-  });
-});
-
-router.post("/", (req, res, next) => {
-  res.status(200).json({
-    message: "Handling POST requests to /products"
-  });
-});
-
-router.get("/:productId", (req, res, next) => {
-  const id = req.params.productId;
-  if (id === "special") {
-    res.status(200).json({
-      message: "You discovered a special ID",
-      id: id
-    });
-  } else {
-    res.status(200).json({
-      message: "You passed an ID"
-    });
+router.get("/", async (req, res, next) => {
+  try {
+    const allProducts = await Product.find();
+    return res.status(200).json({ allProducts });
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
   }
 });
 
-router.patch("/:productId", (req, res, next) => {
-  res.status(200).json({
-    message: "Updated product!"
-  });
+router.post("/", async (req, res, next) => {
+  const { name, price } = req.body;
+
+  try {
+    const product = new Product({
+      _id: new mongoose.Types.ObjectId(),
+      name,
+      price
+    });
+
+    product.save();
+
+    return res.status(201).json({
+      message: "Handling post request on products api",
+      product: product
+    });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ error: error.message });
+  }
 });
 
-router.delete("/:productId", (req, res, next) => {
-  res.status(200).json({
-    message: "Deleted product!"
-  });
+router.get("/:productId", async (req, res, next) => {
+  const id = req.params.productId;
+  try {
+    const product = await Product.findById(id);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    } else {
+      return res.status(200).json({ product });
+    }
+  } catch (err) {
+    console.log(err.message);
+    return res.status(500).json({ error: err.message });
+  }
+});
+
+router.patch("/:productId", async (req, res, next) => {
+  const id = req.params.productId;
+  const { name, price } = req.body;
+  const productFields = {};
+  if (name) productFields.name = name;
+  if (price) productFields.price = price;
+
+  try {
+    let product = await Product.findById(id);
+    if (product)
+      product = await Product.findOneAndUpdate(
+        { _id: id },
+        {
+          $set: productFields
+        },
+        { new: true }
+      );
+    return res.json({ product });
+  } catch (err) {
+    console.log(err.message);
+    return res.status(500).send("Server Error");
+  }
+});
+
+router.delete("/:productId", async (req, res, next) => {
+  const id = req.params.productId;
+  try {
+    const deletedProduct = await Product.remove({ _id: id });
+
+    if (deletedProduct) {
+      return res.status(200).json({ deletedProduct });
+    } else {
+      return res.status(500).json({ message: "No product found with that ID" });
+    }
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 });
 module.exports = router;
--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const cors = require("cors");
+const connectDB = require("./config/db");
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
 
+connectDB();
+
+app.use(cors());
+
+app.use(morgan("dev"));
+app.use(express.json({ extended: false }));
+
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
 
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
 module.exports = app;
